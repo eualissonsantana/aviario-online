285497c8b3928e95e83f06bea1dfb27b7c413cf0
Alisson Santana <alissonsantanapo@gmail.com>
2020-11-12 09:10:33 -0300

    Implementação do método destroy de usuários

--- resources/views/listagem/usuarios.blade.php
+++ resources/views/listagem/usuarios.blade.php
@@ -4,6 +4,7 @@
     <div class="container col-8">
         <h2>Lista de usuários</h2>
         <hr>
+        @csrf
         @foreach ($usuarios as $user)
             <div class="row">
                 <div class="col-10">
@@ -14,6 +15,9 @@
                 <div class="col">
                     <a href="{{url("users/$user->id/edit")}}">
                         <button class="btn btn-primary">Editar</button>
+                    </a>
+                    <a href="{{url("users/$user->id")}}" class="js-del">
+                        <button class="btn btn-danger">Excluir</button>
                     </a>
                 </div>
             </div>
